113c73631602ac9f7ce8b5d8b0db6375d4a35094
Roland Angerer <r.angerer@yelsterdigital.com>
2016-09-21 13:46:43 +0200

    initial commit

---
+++ pokerhands.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# card suits
+# * [C]lubs
+# * [D]iamonds
+# * [H]earts
+# * [S]pades
+
+# card values
+# * 2, 3, 4, 5, 6, 7, 8, 9
+# * T = 10
+# * J = Jack
+# * Q = Queen
+# * K = King
+# * A = Ace
+
+# ranking
+# * high card
+# * pair
+# * two pairs
+# * three of a kind
+# * straight
+# * flush
+# * full house
+# * four of a kind
+# * straight flush
+
+def pokerhands():
+    pass
+
+if __name__ == "__main__":
+    import nose
+    nose.main()
---
+++ test_pokerhands.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from nose.tools import *
+
+from pokerhands import pokerhands
+
+def test_pokerhands():
+    pass
