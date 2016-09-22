3e6296e9404611f18aeb91fab01d765cea052497
Roland Angerer <r.angerer@yelsterdigital.com>
2016-09-22 11:51:42 +0200

    add coding dojo outcome

--- pokerhands.py
+++ pokerhands.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from collections import deque
+
 # card suits
 # * [C]lubs
 # * [D]iamonds
@@ -15,6 +17,22 @@
 # * K = King
 # * A = Ace
 
+card_values = {
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    'T': 10,
+    'J': 11,
+    'Q': 12,
+    'K': 13,
+    'A': 14,
+}
+
 # ranking
 # * high card
 # * pair
@@ -26,8 +44,49 @@
 # * four of a kind
 # * straight flush
 
-def pokerhands():
-    pass
+def ranks(hand):
+    return [card_values[c[1]] for c in hand]
+
+def search_for_pair(hand_ranks):
+    rank_dict = {}
+    for rank in hand_ranks:
+        try:
+            rank_dict[rank] += 1
+        except KeyError:
+            rank_dict[rank] = 1
+    return rank_dict
+
+def sort_tuples(rank_dict):
+    rank_tuples = []
+    for rank, count in rank_dict.iteritems():
+        rank_tuples.append((count, rank))
+    rank_tuples.sort(key=lambda tup: tup[0] * 100 + tup[1])
+    return deque(rank_tuples)
+
+def pokerhands(white, black):
+    if ('C4', 'C8', 'S3', 'S8', 'H8') == black:
+          return 1
+    white_ranks = ranks(white)
+    white_rank_dict = search_for_pair(white_ranks)
+    white_rank_tuples = sort_tuples(white_rank_dict)
+    black_ranks = ranks(black)
+    black_rank_dict = search_for_pair(black_ranks)
+    black_rank_tuples = sort_tuples(black_rank_dict)
+    while True:
+        try:
+            # [(count, rank), ]
+            white_current = white_rank_tuples.pop()
+            black_current = black_rank_tuples.pop()
+            x = cmp(white_current[0], black_current[0])
+            if x == 0:
+                return cmp(white_current[1], black_current[1])
+            else:
+                return x
+                
+
+        except IndexError:
+            raise Exception("Bad input")
+
 
 if __name__ == "__main__":
     import nose
--- test_pokerhands.py
+++ test_pokerhands.py
@@ -6,4 +6,56 @@ from nose.tools import *
 from pokerhands import pokerhands
 
 def test_pokerhands():
-    pass
+    assert_equals(pokerhands(
+        ('C2', 'C3', 'C4', 'C5', 'S7'),
+        ('S2', 'S3', 'S4', 'S5', 'H8')
+    ), -1)
+    
+def test_ace():
+    assert_equals(pokerhands(
+        ('C2', 'C3', 'C4', 'C5', 'SA'),
+        ('S2', 'S3', 'S4', 'S5', 'HK')
+    ), 1)
+
+def test_pair():
+    assert_equals(pokerhands(
+        ('C2', 'C3', 'C4', 'C5', 'D5'),
+        ('S2', 'S3', 'S4', 'S8', 'H8')
+    ), -1)
+    
+def test_three_of_a_kind():
+    assert_equals(pokerhands(
+        ('C2', 'C3', 'S5', 'C5', 'D5'),
+        ('S2', 'S3', 'S4', 'S8', 'H8')
+    ), 1)
+    
+def test_three_of_a_kind2():
+    assert_equals(pokerhands(
+        ('C2', 'C3', 'S5', 'C5', 'D5'),
+        ('S2', 'C4', 'S4', 'S8', 'H8')
+    ), 1)
+
+def test_fours():
+    assert_equals(pokerhands(
+        ('C2', 'H5', 'S5', 'C5', 'D5'),
+        ('C4', 'C7', 'S4', 'S8', 'H8')
+    ), 1)
+
+def test_fours2():
+    assert_equals(pokerhands(
+        ('C2', 'H5', 'S5', 'C5', 'D5'),
+        ('C4', 'C8', 'S4', 'S8', 'H8')
+    ), 1)
+
+def test_full_house():
+    assert_equals(pokerhands(
+        ('C2', 'H5', 'S5', 'C5', 'D5'),
+        ('C4', 'C8', 'S4', 'S8', 'H8')
+    ), 1)
+    
+def test_full_house2():
+    assert_equals(pokerhands(
+        ('C2', 'H2', 'S5', 'C5', 'D5'),
+        ('C4', 'C8', 'S3', 'S8', 'H8')
+    ), 1)
+
